3d5239917331095ba6feb782c22cd06edaa9a8a7
Doug Neiner <doug.neiner@leankit.com>
2015-02-16 15:28:45 -0600

    Better end to end tests

---
+++ spec/examples/istanbul.js
@@ -0,0 +1,3 @@
+define( [ "lodash", "react" ], function( _, React ) {
+	_.each( [ 1, 2, 3 ], console.log.bind( console ) );
+} );
---
+++ spec/examples/multiline.js
@@ -0,0 +1,5 @@
+define( [
+	"lodash"
+], function( _ ) {
+		_.each( [ 1, 2, 3 ], console.log.bind( console ) );
+	} );
